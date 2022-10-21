8220fe0ada80555bc913f65526dacce87b22d444
Eric Song <ericksong91@gmail.com>
2022-10-21 13:23:36 -0400

    Done with assignment

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onDelete, onLike }) {
+  const { id, name, image, likes } = toy;
+
+  function handleClick () {
+    console.log(toy)
+    console.log(id)
+    console.log(`http://localhost:3001/toys/${id}`)
+
+
+
+    const updatedLikes = {
+      "likes": parseInt(likes + 1)
+    };
+
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedLikes)
+    })
+      .then((r) => r.json())
+      .then(onLike)
+
+  }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={()=>handleClick()}>Like {"<3"}</button>
+      <button className="del-btn" onClick={()=>onDelete(id)}>Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys, onDelete, onLike }) {
+
+  const updatedList = toys.map((toy, index) => {
+    return <ToyCard key={`Toy-${index}`} toy={toy} onDelete={onDelete} onLike={onLike} />
+  })
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {updatedList}
+    </div>
   );
 }
 
